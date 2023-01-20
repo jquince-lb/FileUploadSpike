b41f060731f8916bd87487c91e963cf7c58eda08
Joz-Bruer Quince <jquince@lightboxre.com>
2023-01-19 16:13:14 -0800

    Working on the upload function using Multer

---
+++ server/src/middlewares/upload-files.ts
@@ -0,0 +1,18 @@
+// import multer, { diskStorage } from "multer";
+
+// const MIME_TYPES = {
+// 	"image/jpg": "jpg",
+// 	"image/jpeg": "jpeg",
+// 	"image/png": "png",
+// 	"image/gif": "gif",
+// 	"text/plain": "txt",
+// 	"application/pdf": "pdf",
+// };
+
+// const uploadFile = multer({
+//   limits: 500000,
+//   storage: diskStorage({
+
+//   })
+
+// })
